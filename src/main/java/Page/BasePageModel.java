73a8f3bb0d9f3e5f7be9eaf8a554d84beb9d9e2f
kdrozdyk <konrad.drozdyk@sogeti.se>
2019-06-10 14:39:57 +0200

    Latest commit

--- src/main/java/Page/BasePageModel.java
+++ src/main/java/Page/BasePageModel.java
@@ -10,13 +10,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePageModel {
-    protected WebDriver driver;
+    private WebDriver driver;
     protected BasePageModel(WebDriver driver){
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
-    public WebElement getElement(final By locator, int timeOut){
+    private WebElement getElement(final By locator, int timeOut){
         WebElement returnElement;
         if(timeOut < 0 || timeOut > 15){
             timeOut = 15;
@@ -51,15 +51,46 @@ public abstract class BasePageModel {
             System.out.println(locator.toString() + "Has not been found");
         }
     }
+
+    /**
+     *
+     * @param locator Is the path to the element you are trying to reach
+     * @param timeOut Is the time limit in seconds of how long the browser will hold before throwing an error
+     * @param period Is an Enum with values of TODAY, TOMORROW and WEEK
+     * @param format Is the format that you want the date in
+     */
     public void sendDate(final By locator, final int timeOut, DateUtils.period period, String format){
-        String date = "";
         WebElement element = getElement(locator, timeOut);
-        date = DateUtils.returnTheDate(period, format);
-        if (element != null && date != ""){
+        String date = DateUtils.returnTheDate(period, format);
+        if (element != null && !"".equals(date)){
             element.sendKeys(date);
         }
         else {
             System.out.println("Please check that the path to the element is correct and every input is correct.    ");
+        }
+    }
+    public void sendDate(final By locator, final int timeOut, DateUtils.period period){
+        WebElement element = getElement(locator, timeOut);
+        String date = DateUtils.returnTheDate(period);
+        if (element != null && !"".equals(date)){
+            element.sendKeys(date);
+        }
+        else {
+            System.out.println(locator.toString() + "Has not been found");
+        }
+    }
+
+    /**
+     * This method will send todays date in format dd-MM-yyyy
+     */
+    public void sendTodaysDate(final By locator, final int timeOut){
+        WebElement element = getElement(locator, timeOut);
+        String date = DateUtils.getTodaysDate();
+        if (element != null){
+            element.sendKeys(date);
+        }
+        else {
+            System.out.println(locator.toString() + "Has not been located");
         }
     }
     public abstract boolean isLoaded();
--- src/main/java/Utils/DateUtils.java
+++ src/main/java/Utils/DateUtils.java
@@ -1,8 +1,14 @@
 package Utils;
 
+import javax.swing.text.DateFormatter;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 // In progress
@@ -30,20 +36,48 @@ public class DateUtils {
         }
         return ofFormat(date, format);
     }
-    public static String getTodaysDate(){
-        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+    public static String returnTheDate(period period){
+        String date;
+        switch (period){
+            case TODAY:
+                date=getTodaysDate();
+                break;
+            case TOMOROW:
+                date=getTomorrowsDate();
+                break;
+            case WEEK:
+                date=getOneWeekFromToday();
+                break;
+            default:
+                date=getTodaysDate();
+                break;
+        }
         return date;
+    }
+    public static LocalDate getTodaysDate(){
+        //String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        return LocalDate.now();
     }
     public static String getTomorrowsDate(){
         String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
         return LocalDate.parse(date).plusDays(1).toString();
     }
     public static String getOneWeekFromToday(){
-        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
-        return LocalDate.parse(date).plusDays(7).toString();
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 7);
+        return cal.getTime().toString();
     }
-    public static String ofFormat(String date, String format){
-        return LocalDate.parse(date, DateTimeFormatter.ofPattern(format)).toString();
+    public static String ofFormat(String format, LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        return localDate.toString();
     }
     public enum period{TODAY, TOMOROW, WEEK;}
+
+   /* public static String getCustomDate(int daysFromToday, String format){
+
+    }*/
+    public static void main(String[] args){
+        System.out.println(returnTheDate(period.TODAY, "dd/MM/uuuu"));
+    }
 }
