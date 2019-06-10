374b0601f4561113f2d9cb3bb306bf4f3f15b77d
kdrozdyk <konrad.drozdyk@sogeti.se>
2019-06-10 09:53:09 +0200

    Latest commit

---
+++ src/main/java/Utils/DateUtils.java
@@ -0,0 +1,17 @@
+package Utils;
+
+// In progress
+public class DateUtils {
+    /**
+     * This class will return a date of today, tomorow or one week from today based on your preference
+     * @param format Here you can state which format you want for your date; Acceptable formats are as follows:
+     *               dd-MM-YYYY, YYYY-MM-dd, MM-dd-YYYY, YYYY-dd-MM, MM-dd, dd-MM
+     */
+    public String getFormat (String format){
+
+    }
+    public String returnTheDate(){
+
+    }
+    public enum period{TODAY, TOMOROW, WEEK;}
+}
